dcbfa78850efe8dac341fbb428bd9002f53abd40
PedroSousaCoelho <pedrosousacoelhoo@gmail.com>
2022-11-29 22:10:28 -0300

    add teste

--- src/teste.jsx
+++ src/teste.jsx
@@ -1,18 +1,38 @@
-import {useState} from 'react'
+import {useState, useEffect, useMemo} from 'react'
 
 const Teste = () => { 
-    const [name, setName] = useState('Pedro')
+    const [age, setAge] = useState(19)
 
     const handleChangeName = () => { 
-        setName('Pedro')
+        setAge(prev => prev === 30 ? 19 : 30)
     }
+
+   const calculo = useMemo(() => { 
+    console.log('Calculou', age)
+    return 10 * age
+   }, [])
+
+    
+
     return (
         <div>
-            <p>{name}</p>
-            <button onClick={handleChangeName}>Alterar</button>
+            <p>{age}</p>
+            <button onClick={calculo}>Alterar</button>
         </div>
     )
 }
 
 export {Teste}
 
+
+/*
+ USE STATE ARMAZENAMENTO DE STATUS E PARA E ALTERACAO NA APLICACAO
+ USE STATE GERENCIAR O ESTADO DE UM COMPONENTE
+
+ HOOKS PERMITEM O USO DE VARIOS RECURSOS DE FORMA SIMPLES NA FUNCAO
+
+ USE EFFECT -> HOOK PARA GERENCIAR O CICLO DE VIDA DE UM COMPONENTE
+
+ USE MEMO -> REFERENCIA EM VARIAVEIS
+ USE CALLBACK -> REFERENCIA DE FUNCAO
+ */
